c8b2cf2f1e8826ad4f1782b009fe5f914a730f7b
Mustafa Biçer <mustafa.bicer@segmentify.com>
2017-08-04 22:23:34 +0300

    PUSH

--- training.js
+++ training.js
@@ -3,3 +3,9 @@ var app = express();
 app.get('/api/test', function (req, res) {
     res.end("HELLO WORLD!");
 });
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+})
