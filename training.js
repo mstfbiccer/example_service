c3b19183c339a135348033d78b977878514a527d
Mustafa Biçer <mustafa.bicer@segmentify.com>
2017-08-04 22:15:51 +0300

    STAT

--- training.js
+++ training.js
@@ -1,6 +1,5 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080);
+var express = require('express');
+var app = express();
+app.get('/api/test', function (req, res) {
+    res.end("HELLO WORLD!");
+});
